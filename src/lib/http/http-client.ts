b94659efee13cf8e9b8809d352f5544a45ca622a
ByeongGi <wwww3426@naver.com>
2025-03-29 02:10:45 +0900

    feat: add http client

---
+++ src/lib/http/http-client.ts
@@ -0,0 +1,32 @@
+import axios, { isAxiosError } from 'axios';
+
+
+
+const getErrorMessage = (error: unknown) => {
+  if (isAxiosError(error)) {
+    return error.response?.data?.message || error.message;
+  }
+  return String(error);
+}
+
+
+const createHttpClient = () => {
+  // axios 인스턴스 생성
+  const instance = axios.create({
+    // baseURL: process.env.NEXT_PUBLIC_API_URL,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    withCredentials: true,
+  });
+  return instance;
+}
+
+
+// HTTP 메서드 함수들
+const httpClient = createHttpClient();
+
+
+export {
+  httpClient,
+}
